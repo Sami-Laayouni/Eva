513c7b67d825a74277c0ea8c71e5f2e1b5b39b9c
Sami Laayouni <sam.laayouni@aui.ma>
2024-03-15 22:57:15 +0000

    ci: key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  ? process.env.NEXT_PUBLIC_SUPABASE_URL
+  : "example";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  : "example2";
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL or key is not defined.");
