35bac03cbd7b9bf91273f1e575ab3c621d48b040
Sami Laayouni <sam.laayouni@aui.ma>
2024-03-15 23:03:25 +0000

    ci: fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,10 +2,10 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
   ? process.env.NEXT_PUBLIC_SUPABASE_URL
-  : "example";
+  : "https://fhbwfjpjrvdmslmjdips.supabase.co";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  : "example2";
+  : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoYndmanBqcnZkbXNsbWpkaXBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ0NzMxNjEsImV4cCI6MjAyMDA0OTE2MX0.dQuKc1I3f-qJARPlAaoPNUUMwuOwIrpJqbH10WFPHbI";
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL or key is not defined.");
